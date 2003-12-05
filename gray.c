657bbe6b81bd5afb50f6fe9f1e0be57af7da196b
Urs Thuermann <urs@isnogud.escape.de>
2003-12-05 13:55:35 +0100

    Add command-line argument for number of Gray codes to print

--- gray.c
+++ gray.c
@@ -1,14 +1,19 @@
 /*
- * $Id: gray.c,v 1.1 1998/01/26 18:01:23 urs Exp $
+ * $Id: gray.c,v 1.2 2003/12/05 12:55:35 urs Exp $
  */
 
+#include <stdlib.h>
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
+    int N = 16;
     int n;
 
-    for (n = 0 ; n < 16; n++)
+    if (argc > 1)
+	N = atoi(argv[1]);
+
+    for (n = 0; n < N; n++)
 	printf("%2x: %2x\n", n, n ^ (n >> 1));
 
     return 0;
