ccf33eeb61f0abfbeae7abe25c72a424e5cf4820
Urs Thuermann <urs@isnogud.escape.de>
2014-06-30 13:01:06 +0200

    Print Gray code in decimal and full binary, and align columns

--- gray.c
+++ gray.c
@@ -1,20 +1,24 @@
 /*
- * $Id: gray.c,v 1.4 2014/06/30 11:00:56 urs Exp $
+ * $Id: gray.c,v 1.5 2014/06/30 11:01:06 urs Exp $
  */
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
 
 static void usage(const char *name)
 {
     fprintf(stderr, "Usage: %s [min] max\n", name);
 }
 
+static unsigned long gray(unsigned long n);
+static int length(unsigned long n, int base);
+static char *bin(unsigned long n, size_t len);
+
 int main(int argc, char **argv)
 {
-    unsigned long n, m, min, max;
-    int i;
-    char bits[32 + 1];
+    unsigned long n, min, max;
+    int wd, wb;
 
     if (argc != 2 && argc != 3) {
 	usage(argv[0]);
@@ -28,13 +32,43 @@ int main(int argc, char **argv)
 	max = strtoul(argv[2], NULL, 0);
     }
 
-    for (n = min; n < max; n++) {
-	m = n ^ (n >> 1);
-	for (i = 0; i < 8; i++)
-	    bits[i] = '0' + ((m & (1 << (7 - i))) != 0);
-	bits[i] = 0;
-	printf("%2lx: %2lx %s\n", n, m, bits);
-    }
+    wd = length(max - 1, 10);
+    wb = length(max - 1, 2);
+
+    for (n = min; n < max; n++)
+	printf("%*lu %*lu %s\n", wd, n, wd, gray(n), bin(gray(n), wb));
 
     return 0;
+}
+
+static unsigned long gray(unsigned long n)
+{
+    return n ^ (n >> 1);
+}
+
+static int length(unsigned long n, int base)
+{
+    int len = 0;
+
+    do {
+	n /= base;
+	len++;
+    } while (n > 0);
+
+    return len;
+}
+
+#define MAXLEN (sizeof(unsigned long) * CHAR_BIT)
+
+static char *bin(unsigned long n, size_t len)
+{
+    static char s[MAXLEN + 1];
+    int i = MAXLEN;
+
+    do {
+	s[--i] = n & 1 ? '1' : '0';
+	n >>= 1;
+    } while (n || MAXLEN - i < len);
+
+    return s + i;
 }
