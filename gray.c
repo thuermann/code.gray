93ed649d29bdaa9166b621c94bc46d4130b6894f
Urs Thuermann <urs@isnogud.escape.de>
2014-06-30 13:00:56 +0200

    Allow start value to be given on command line and check for usage

--- gray.c
+++ gray.c
@@ -1,25 +1,39 @@
 /*
- * $Id: gray.c,v 1.3 2003/12/05 12:55:45 urs Exp $
+ * $Id: gray.c,v 1.4 2014/06/30 11:00:56 urs Exp $
  */
 
 #include <stdlib.h>
 #include <stdio.h>
 
+static void usage(const char *name)
+{
+    fprintf(stderr, "Usage: %s [min] max\n", name);
+}
+
 int main(int argc, char **argv)
 {
-    int N = 16;
-    int n, m, i;
+    unsigned long n, m, min, max;
+    int i;
     char bits[32 + 1];
 
-    if (argc > 1)
-	N = atoi(argv[1]);
+    if (argc != 2 && argc != 3) {
+	usage(argv[0]);
+	exit(1);
+    }
 
-    for (n = 0; n < N; n++) {
+    min = 0;
+    max = strtoul(argv[1], NULL, 0);
+    if (argc == 3) {
+	min = max;
+	max = strtoul(argv[2], NULL, 0);
+    }
+
+    for (n = min; n < max; n++) {
 	m = n ^ (n >> 1);
 	for (i = 0; i < 8; i++)
 	    bits[i] = '0' + ((m & (1 << (7 - i))) != 0);
 	bits[i] = 0;
-	printf("%2x: %2x %s\n", n, m, bits);
+	printf("%2lx: %2lx %s\n", n, m, bits);
     }
 
     return 0;
